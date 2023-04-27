007353707e922f74948607c33aff8e97e7bf23b8
Xelork <101120856+Xelork@users.noreply.github.com>
2023-04-27 01:35:54 -0300

    Atualizar 'main.blocks, main.ts, main.py'

--- main.py
+++ main.py
@@ -1,181 +1,300 @@
-/**
- * game 0 = title
- * 
- * game 1 = load
- * 
- * game 2 = game thinker
- * 
- * game 3 = game over
- * 
- * game 4 = show score
- * 
- * go back to 0
- */
-/**
- * WHY -Y GOES UP???
- * 
- * AND VICE-VERSA!!!
- */
-/**
- * snakedir:
- * 
- * 6 = right
- * 
- * 8 = up
- * 
- * 4 = left
- * 
- * 2 = down
- * 
- * just like the numpad!
- */
-function menu () {
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.ringTone(440)
+"""
+
+WHY -Y GOES UP???
+
+AND VICE-VERSA!!!
+
+"""
+"""
+
+game 0 = title
+
+game 1 = load
+
+game 2 = game thinker
+
+game 3 = game over
+
+game 4 = show score
+
+go back to 0
+
+"""
+def gameover_jingle():
+    music.play_tone(175, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.QUARTER))
+    music.play_tone(175, music.beat(BeatFraction.QUARTER))
+    music.play_tone(196, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.QUARTER))
+    music.play_tone(196, music.beat(BeatFraction.QUARTER))
+    music.play_tone(220, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.QUARTER))
+    music.play_tone(220, music.beat(BeatFraction.QUARTER))
+    music.play_tone(247, music.beat(BeatFraction.QUARTER))
+    music.play_tone(220, music.beat(BeatFraction.QUARTER))
+    music.play_tone(196, music.beat(BeatFraction.QUARTER))
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(196, music.beat(BeatFraction.QUARTER))
+    music.play_tone(131, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.SIXTEENTH))
+def menu():
+    music.play_tone(466, music.beat(BeatFraction.WHOLE))
+    music.play_tone(440, music.beat(BeatFraction.WHOLE))
+    music.play_tone(466, music.beat(BeatFraction.WHOLE))
+    music.ring_tone(440)
     basic.pause(750)
-    music.ringTone(392)
+    music.ring_tone(392)
     basic.pause(1500)
-    music.rest(music.beat(BeatFraction.Half))
-    music.ringTone(440)
+    music.rest(music.beat(BeatFraction.HALF))
+    music.ring_tone(440)
     basic.pause(375)
-    music.ringTone(466)
+    music.ring_tone(466)
     basic.pause(375)
-    music.ringTone(523)
+    music.ring_tone(523)
     basic.pause(375)
-    music.ringTone(440)
+    music.ring_tone(440)
     basic.pause(375)
-    music.playTone(392, music.beat(BeatFraction.Quarter))
-    music.rest(music.beat(BeatFraction.Quarter))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.ringTone(349)
+    music.play_tone(392, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.QUARTER))
+    music.play_tone(392, music.beat(BeatFraction.WHOLE))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.ring_tone(349)
     basic.pause(1500)
-    music.rest(music.beat(BeatFraction.Half))
-    music.ringTone(294)
+    music.rest(music.beat(BeatFraction.HALF))
+    music.ring_tone(294)
     basic.pause(375)
-    music.ringTone(311)
+    music.ring_tone(311)
     basic.pause(375)
-    music.ringTone(349)
+    music.ring_tone(349)
     basic.pause(375)
-    music.ringTone(392)
+    music.ring_tone(392)
     basic.pause(375)
-    music.playTone(294, music.beat(BeatFraction.Quarter))
-    music.rest(music.beat(BeatFraction.Quarter))
-    music.ringTone(349)
+    music.play_tone(294, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.QUARTER))
+    music.ring_tone(349)
     basic.pause(625)
-    music.rest(music.beat(BeatFraction.Quarter))
-    music.ringTone(311)
+    music.rest(music.beat(BeatFraction.QUARTER))
+    music.ring_tone(311)
     basic.pause(750)
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.ringTone(349)
+    music.play_tone(466, music.beat(BeatFraction.WHOLE))
+    music.play_tone(440, music.beat(BeatFraction.WHOLE))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.WHOLE))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(440, music.beat(BeatFraction.WHOLE))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.ring_tone(349)
     basic.pause(1500)
-}
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (game2 == 0) {
-        game2 = 1
-        music.ringTone(523)
+
+def on_logo_pressed():
+    global gamestate, pause2
+    if gamestate == 0:
+        basic.clear_screen()
+        gamestate = 1
+        music.ring_tone(523)
         basic.pause(125)
-        music.ringTone(440)
+        music.ring_tone(440)
         basic.pause(375)
-        music.stopAllSounds()
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    if (game2 == 2) {
-        if (snakedir == 6) {
+        music.rest(1)
+    elif gamestate == 2:
+        if pause2 == 0:
+            pause2 = 1
+            pause_sfx()
+        else:
+            pause2 = 0
+            pause_sfx()
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def on_button_pressed_a():
+    global snakedir, snakedirx, snakediry
+    if gamestate == 2:
+        if snakedir == 6:
             snakedir = 8
             snakedirx += -1
             snakediry += -1
-        } else if (snakedir == 8) {
+        elif snakedir == 8:
             snakedir = 4
             snakedirx += -1
             snakediry += 1
-        } else if (snakedir == 4) {
+        elif snakedir == 4:
             snakedir = 2
             snakedirx += 1
             snakediry += 1
-        } else {
+        else:
             snakedir = 6
             snakedirx += 1
             snakediry += -1
-        }
-    }
-})
-input.onPinPressed(TouchPin.P2, function () {
-    music.setVolume(music.volume() + 16)
-})
-input.onButtonPressed(Button.B, function () {
-    if (game2 == 2) {
-        if (snakedir == 6) {
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def pause_sfx():
+    music.ring_tone(988)
+    basic.pause(100)
+    music.ring_tone(784)
+    basic.pause(100)
+    music.ring_tone(988)
+    basic.pause(100)
+    music.ring_tone(784)
+    basic.pause(100)
+    music.stop_all_sounds()
+
+def on_pin_pressed_p2():
+    music.set_volume(music.volume() + 16)
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def on_button_pressed_b():
+    global snakedir, snakedirx, snakediry
+    if gamestate == 2:
+        if snakedir == 6:
             snakedir = 2
             snakedirx += -1
             snakediry += 1
-        } else if (snakedir == 2) {
+        elif snakedir == 2:
             snakedir = 4
             snakedirx += -1
             snakediry += -1
-        } else if (snakedir == 4) {
+        elif snakedir == 4:
             snakedir = 8
             snakedirx += 1
             snakediry += -1
-        } else {
+        else:
             snakedir = 6
             snakedirx += 1
             snakediry += 1
-        }
-    }
-})
-input.onPinPressed(TouchPin.P1, function () {
-    music.setVolume(music.volume() - 16)
-})
-function snake () {
-    snakex += snakedirx
-    snakey += snakediry
-    led.plot(snakex, snakey)
-}
-let lista: boolean[] = []
-let snakey = 0
-let snakex = 0
-let snakedirx = 0
-let snakediry = 0
-let snakedir = 0
-let game2 = 0
-music.stopAllSounds()
-music.setVolume(127)
-game2 = 0
-snakedir = 6
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_pin_pressed_p1():
+    music.set_volume(music.volume() - 16)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+def snake():
+    global sfx, applex, appley, snakex, snakey, gamestate
+    if led.point_brightness(applex, appley) < 32:
+        led.plot_brightness(applex, appley, 32)
+    if snakex == applex and snakey == appley:
+        sfx = 1
+        applex = randint(0, 4)
+        appley = randint(0, 4)
+        led.plot_brightness(applex, appley, 32)
+        oldsnakepos.append(snakex)
+        oldsnakepos.append(snakey)
+    if not (snakex < 0 or snakex > 4 or (snakey < 0 or snakey > 4)):
+        led.unplot(oldsnakepos.shift(), oldsnakepos.shift())
+        snakex += snakedirx
+        snakey += snakediry
+        oldsnakepos.append(snakex)
+        oldsnakepos.append(snakey)
+        if led.point_brightness(snakex, snakey) != 255:
+            led.plot(snakex, snakey)
+        else:
+            gamestate = 3
+    else:
+        gamestate = 3
+oldsnakepos: List[number] = []
+sfx = 0
+snakey = 0
+snakex = 0
+appley = 0
+applex = 0
 snakediry = 0
-snakedirx = 1
-let pause2 = 0
-basic.forever(function () {
-    if (game2 == 0) {
-    	
-    } else if (game2 == 1) {
+snakedirx = 0
+snakedir = 0
+pause2 = 0
+gamestate = 0
+# snakedir:
+# 
+# 6 = right
+# 
+# 8 = up
+# 
+# 4 = left
+# 
+# 2 = down
+# 
+# just like the numpad!
+music.ring_tone(988)
+music.stop_all_sounds()
+music.set_volume(127)
+gamestate = 0
+pause2 = 0
+
+def on_forever():
+    global snakex, snakey, snakedir, snakediry, snakedirx, applex, appley, oldsnakepos, sfx, gamestate
+    if gamestate == 0:
+        basic.show_string("SNAKE")
+    elif gamestate == 1:
         basic.pause(800)
-        led.plot(1, 2)
         snakex = 1
         snakey = 2
-        game2 += 1
-        lista = [snakex && snakey]
-    } else {
-    	
-    }
-})
-control.inBackground(function () {
-	
-})
-loops.everyInterval(800, function () {
-    if (game2 == 2) {
-        snake()
-    }
-})
+        snakedir = 6
+        snakediry = 0
+        snakedirx = 1
+        applex = randint(0, 4)
+        appley = randint(0, 4)
+        led.plot_brightness(applex, appley, 64)
+        oldsnakepos = []
+        sfx = 0
+        gamestate += 1
+    elif gamestate == 2:
+        if pause2 == 0:
+            snake()
+            if sfx == 1:
+                sfx = 0
+                music.ring_tone(523)
+                basic.pause(100)
+                music.ring_tone(587)
+                basic.pause(100)
+                music.ring_tone(523)
+                basic.pause(300)
+                music.rest(1)
+            basic.pause(800)
+    elif gamestate == 3:
+        basic.clear_screen()
+        basic.pause(250)
+        basic.show_leds("""
+            . # # # .
+                        # . # . #
+                        . # . # .
+                        . # # # .
+                        . # . # .
+        """)
+        music.ring_tone(262)
+        basic.pause(250)
+        music.ring_tone(247)
+        basic.pause(125)
+        music.ring_tone(233)
+        basic.pause(250)
+        music.ring_tone(220)
+        basic.pause(125)
+        music.ring_tone(208)
+        basic.pause(250)
+        music.ring_tone(156)
+        basic.pause(34)
+        music.rest(68)
+        music.ring_tone(156)
+        basic.pause(273)
+        music.ring_tone(208)
+        basic.pause(125)
+        music.ring_tone(196)
+        basic.pause(750)
+        music.ring_tone(349)
+        basic.pause(33)
+        music.ring_tone(370)
+        basic.pause(33)
+        music.ring_tone(392)
+        basic.pause(184)
+        music.rest(1)
+        basic.pause(1000)
+        gamestate = 4
+    else:
+        basic.show_number((len(oldsnakepos) - 2) / 2)
+        basic.pause(2000)
+        basic.clear_screen()
+        gamestate = 0
+basic.forever(on_forever)
+
+def on_in_background():
+    pass
+control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -16,19 +16,25 @@
  * 
  * AND VICE-VERSA!!!
  */
-/**
- * snakedir:
- * 
- * 6 = right
- * 
- * 8 = up
- * 
- * 4 = left
- * 
- * 2 = down
- * 
- * just like the numpad!
- */
+function gameover_jingle () {
+    music.playTone(175, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(175, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(247, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
 function menu () {
     music.playTone(466, music.beat(BeatFraction.Whole))
     music.playTone(440, music.beat(BeatFraction.Whole))
@@ -81,17 +87,26 @@ function menu () {
     basic.pause(1500)
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (game2 == 0) {
-        game2 = 1
+    if (gamestate == 0) {
+        basic.clearScreen()
+        gamestate = 1
         music.ringTone(523)
         basic.pause(125)
         music.ringTone(440)
         basic.pause(375)
-        music.stopAllSounds()
+        music.rest(1)
+    } else if (gamestate == 2) {
+        if (pause2 == 0) {
+            pause2 = 1
+            pause_sfx()
+        } else {
+            pause2 = 0
+            pause_sfx()
+        }
     }
 })
 input.onButtonPressed(Button.A, function () {
-    if (game2 == 2) {
+    if (gamestate == 2) {
         if (snakedir == 6) {
             snakedir = 8
             snakedirx += -1
@@ -111,11 +126,25 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+function pause_sfx () {
+    music.ringTone(988)
+    basic.pause(100)
+    music.ringTone(784)
+    basic.pause(100)
+    music.ringTone(988)
+    basic.pause(100)
+    music.ringTone(784)
+    basic.pause(100)
+    music.stopAllSounds()
+}
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_EVT_ANY, function () {
+    gameover_jingle()
+})
 input.onPinPressed(TouchPin.P2, function () {
     music.setVolume(music.volume() + 16)
 })
 input.onButtonPressed(Button.B, function () {
-    if (game2 == 2) {
+    if (gamestate == 2) {
         if (snakedir == 6) {
             snakedir = 2
             snakedirx += -1
@@ -139,46 +168,128 @@ input.onPinPressed(TouchPin.P1, function () {
     music.setVolume(music.volume() - 16)
 })
 function snake () {
-    led.unplot(lista.shift(), lista.shift())
-    snakex += snakedirx
-    snakey += snakediry
-    lista.push(snakex)
-    lista.push(snakey)
-    led.plot(snakex, snakey)
+    if (led.pointBrightness(applex, appley) < 16) {
+        led.plotBrightness(applex, appley, 16)
+    }
+    if (snakex == applex && snakey == appley) {
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_IO_P1,
+        EventBusValue.MICROBIT_EVT_ANY
+        )
+        applex = randint(0, 4)
+        appley = randint(0, 4)
+        led.plotBrightness(applex, appley, 16)
+        oldsnakepos.push(snakex)
+        oldsnakepos.push(snakey)
+    }
+    if (!(snakex < 0 || snakex > 4 || (snakey < 0 || snakey > 4))) {
+        led.unplot(oldsnakepos.shift(), oldsnakepos.shift())
+        snakex += snakedirx
+        snakey += snakediry
+        oldsnakepos.push(snakex)
+        oldsnakepos.push(snakey)
+        if (led.pointBrightness(snakex, snakey) != 255) {
+            led.plot(snakex, snakey)
+        } else {
+            gamestate = 3
+        }
+    } else {
+        gamestate = 3
+    }
 }
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P1, EventBusValue.MICROBIT_EVT_ANY, function () {
+    music.ringTone(523)
+    basic.pause(100)
+    music.ringTone(587)
+    basic.pause(100)
+    music.ringTone(523)
+    basic.pause(300)
+    music.rest(1)
+})
+let sfx = 0
+let oldsnakepos: number[] = []
 let snakey = 0
 let snakex = 0
-let lista: number[] = []
-let snakedirx = 0
+let appley = 0
+let applex = 0
 let snakediry = 0
+let snakedirx = 0
 let snakedir = 0
-let game2 = 0
+let pause2 = 0
+let gamestate = 0
 music.stopAllSounds()
 music.setVolume(127)
-game2 = 0
-snakedir = 6
-snakediry = 0
-snakedirx = 1
-let pause2 = 0
+gamestate = 0
+pause2 = 0
 basic.forever(function () {
-    if (game2 == 0) {
-    	
-    } else if (game2 == 1) {
+    if (gamestate == 0) {
+        basic.showString("SNAKE")
+    } else if (gamestate == 1) {
         basic.pause(800)
-        led.plot(1, 2)
         snakex = 1
         snakey = 2
-        game2 += 1
-        lista = [snakex, snakey]
+        snakedir = 6
+        snakediry = 0
+        snakedirx = 1
+        applex = randint(0, 4)
+        appley = randint(0, 4)
+        led.plotBrightness(applex, appley, 16)
+        oldsnakepos = []
+        sfx = 0
+        gamestate += 1
+    } else if (gamestate == 2) {
+        if (pause2 == 0) {
+            snake()
+            basic.pause(800)
+        }
+    } else if (gamestate == 3) {
+        basic.clearScreen()
+        basic.pause(250)
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            . # . # .
+            . # # # .
+            . # . # .
+            `)
+        music.ringTone(262)
+        basic.pause(250)
+        music.ringTone(247)
+        basic.pause(125)
+        music.ringTone(233)
+        basic.pause(250)
+        music.ringTone(220)
+        basic.pause(125)
+        music.ringTone(208)
+        basic.pause(250)
+        music.ringTone(156)
+        basic.pause(34)
+        music.rest(68)
+        music.ringTone(156)
+        basic.pause(273)
+        music.ringTone(208)
+        basic.pause(125)
+        music.ringTone(196)
+        basic.pause(750)
+        music.ringTone(349)
+        basic.pause(33)
+        music.ringTone(370)
+        basic.pause(33)
+        music.ringTone(392)
+        basic.pause(184)
+        music.rest(1)
+        basic.pause(1000)
+        gamestate = 4
     } else {
-    	
-    }
-})
-control.inBackground(function () {
-	
-})
-loops.everyInterval(800, function () {
-    if (game2 == 2) {
-        snake()
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_IO_P0,
+        EventBusValue.MICROBIT_EVT_ANY
+        )
+        for (let index = 0; index < 3; index++) {
+            basic.showNumber((oldsnakepos.length - 2) / 2)
+        }
+        basic.pause(2000)
+        basic.clearScreen()
+        gamestate = 0
     }
 })
